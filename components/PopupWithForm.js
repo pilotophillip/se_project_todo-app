b9ac22350c3ef771eddf673514de6db5b807c1f9
phillip <pilotophillip@gmail.com>
2025-07-26 02:59:21 -0700

    P8 P7 Form Submission

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -3,6 +3,32 @@ import Popup from "./Popup.js";
 class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = this._popupForm.querySelectorAll(".popup__input"); // Moved this here to avoid querying repeatedly
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputList.forEach((input) => {
+      // Add a key/value pair to the values object for each input
+      // Key is input.name; value is input.value
+      // Use bracket notation to avoid hardcoding property names
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      const inputValues = this._getInputValues();
+
+      // Pass result of _getInputValues to submission handler
+      this._handleFormSubmit(inputValues); // Pass the input values
+    });
   }
 }
+
 export default PopupWithForm;
--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,32 @@ const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
 
-// Create your PopupWithForm instance
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
-  handleFormSubmit: () => {},
+  handleFormSubmit: (inputValues) => {
+    const name = inputValues.name;
+    const dateInput = inputValues.date;
+
+    const date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+    const id = uuidv4();
+    const values = { name, date, id };
+
+    const todo = generateTodo(values);
+    section.addItem(todo);
+
+    addTodoPopup.close();
+    addTodoForm.reset();
+  },
 });
 
-// --- DEFINE generateTodo BEFORE using it ---
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
-  return todo.getView();
+  const todoElement = todo.getView();
+  return todoElement;
 };
 
-// Create Section instance, passing initialTodos and renderer
 const section = new Section(
   {
     items: initialTodos,
@@ -34,37 +47,13 @@ const section = new Section(
   ".todos__list"
 );
 
-// Render all initial todos
 section.renderItems();
 
-// Setup popup event listeners
 addTodoPopup.setEventListeners();
 
-// Open popup when add button clicked
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
 
-// Handle form submission
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  const date = new Date(dateInput);
-  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-  const id = uuidv4();
-  const values = { name, date, id };
-
-  const todo = generateTodo(values);
-  section.addItem(todo);
-
-  addTodoPopup.close();
-  addTodoForm.reset(); // Clear form inputs after submit
-});
-
-// Enable validation on the form
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
 newTodoValidator.enableValidation();
